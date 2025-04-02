c7c3418704f359a4f4a62781d92e075d0d77acca
rodrigoFM17 <rodrig4243@gmail.com>
2025-04-01 19:06:43 -0600

    adicion ClientRepository

--- app/src/main/java/com/example/sgprepartidor/Clients/data/repository/ClientRepository.kt
+++ app/src/main/java/com/example/sgprepartidor/Clients/data/repository/ClientRepository.kt
@@ -1,4 +1,14 @@
 package com.example.sgprepartidor.Clients.data.repository
 
-class ClientRepository {
+import android.content.Context
+import com.example.sgprepartidor.core.data.local.Client.entities.ClientEntity
+import com.example.sgprepartidor.core.data.local.appDatabase.DatabaseProvider
+
+class ClientRepository(context: Context) {
+
+    private val clientDAO = DatabaseProvider.getDatabase(context).clientDAO()
+
+    suspend fun insertClient(client: ClientEntity) {
+        clientDAO.insertClient(client)
+    }
 }
